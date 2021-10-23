02f2c1482409bde938da43d5fa61e2bf5b022190
Miłosz Paweł <p_milosz@ms.wwsi.edu.pl>
2021-10-23 16:06:39 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {Console.WriteLine("Podaj swoje imiê:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            int number = 15;
+
+            CreateHostBuilder(args).Build().Run();
+        }
+        
+            
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
